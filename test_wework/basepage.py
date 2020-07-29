25150380c5369ba540f6f7abfa0a27dcaf7bb40f
lihongyu <lihongyu@qqikids.com>
2020-07-29 19:45:01 +0800

    selenium test

--- test_wework/basepage.py
+++ test_wework/basepage.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.android.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 
 
 class BasePage:
-
+    driver = None
     _base_url = ''
 
-    def __init__(self, driver=None):
+    def __init__(self, driver: WebDriver = None):
         if driver is None:
             options = Options()
             options.debugger_address = "127.0.0.1:9222"
             self.driver = webdriver.Chrome(options=options)
-
+            print(f"1111111111=========={driver}")
             self.driver.maximize_window()
             self.driver.implicitly_wait(3)
         else:
             self.driver = driver
+            print(f"222222222=========={driver}")
+            print(f"333333333=========={BasePage.driver}")
 
         if self._base_url != "":
             self.driver.get(self._base_url)
--- test_wework/test_class.py
+++
@@ -1,8 +0,0 @@
-class TestDemo:
-    _a = None
-
-    def test_a(self):
-        self._a = "ABC"
-        print(TestDemo._a)
-        print(self._a)
-
